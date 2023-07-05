5ea7300e874339ccb1f7df534226026cef5d53f9
Mila <milaivanova2609@gmail.com>
2023-07-05 22:03:46 +0300

    Completed

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
-namespace ASP
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+var app = builder.Build();
+
+app.Run(async (context) =>
 {
-    public class Program
+    var request = context.Request;
+    var path = context.Request.Path;
+    context.Response.ContentType = "text/html; charset=utf-8";
+
+    switch (path)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
-        }
+        case "/info":
+            await context.Response.WriteAsync($"Host: {request.Host}, Path: {request.Path}, QueryString: {request.QueryString}");
+            break;
+        case "/time":
+            await context.Response.WriteAsync($"DateTime: {DateTime.Now}");
+            break;
+        case "/key":
+            string value = app.Configuration["key"];
+            await context.Response.WriteAsync($"Value: {value}");
+            break;
+        default:
+            await context.Response.WriteAsync("Привіт користувачеві NET 6.0");
+            break;
     }
-}
+}); 
+
+app.Run();
