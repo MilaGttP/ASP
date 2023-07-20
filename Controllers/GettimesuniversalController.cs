c5f43d4302d21a49c75ec7470963f1174a262634
Mila <milaivanova2609@gmail.com>
2023-07-20 16:38:39 +0300

    Completed

---
+++ Controllers/GettimesuniversalController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP
+{
+    public class GettimesuniversalController : Controller
+    {
+        public IActionResult GetUtcTimes()
+        {
+            return Content($"GetUtcTimes: {DateTime.UtcNow.ToShortTimeString()}");
+        }
+
+
+        //GetCurrentTimes (date): /Gettimesuniversal/GetCurrentTimes?parameter=d
+        //GetCurrentTimes (time): /Gettimesuniversal/GetCurrentTimes?parameter=t
+        //GetCurrentTimes (date and time): /Gettimesuniversal/GetCurrentTimes
+
+        public IActionResult GetCurrentTimes(char parameter)
+        {
+            if (parameter == 'd')
+            {
+                return Content($"GetCurrentTimes (date): {DateTime.Now.ToShortDateString()}");
+            }
+            else if (parameter == 't')
+            {
+                return Content($"GetCurrentTimes (time): {DateTime.Now.ToShortTimeString()}");
+            }
+            else
+            {
+                return Content($"GetCurrentTimes (date and time): {DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss")}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 
+using ASP;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute (
+    name: "default",
+    pattern: "{controller=Gettimesuniversal}/{action=GetUtcTimes}"
+);
 
 app.Run();
