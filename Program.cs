7db8b0397baa533e9569e6a0087c42dfd0c5f090
Mila <milaivanova2609@gmail.com>
2023-07-20 14:49:53 +0300

    wwwroot added

--- Program.cs
+++ Program.cs
@@ -1,15 +1,7 @@
-namespace ASP
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-            app.Run();
-        }
-    }
-}
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
