67353461143017490eab36e8e7f3a816fd5c2bb4
Mila <milaivanova2609@gmail.com>
2023-09-09 13:40:09 +0300

    Initital

--- Program.cs
+++ Program.cs
@@ -1,15 +1,6 @@
-namespace ASP
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!");
 
-            app.Run();
-        }
-    }
-}
+app.Run();
